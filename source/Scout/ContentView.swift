de1a8bb792a507bfdea50771b4bfc036ef2629b9
Ryan Barger <Ryan@Barger.com>
2022-05-23 07:01:57 -0400

    Initial check-in of source code

---
+++ source/Scout/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Scout
+//
+//  Created by Ryan Barger on 5/23/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
